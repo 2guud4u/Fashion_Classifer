5aba7166c486fdfda4683b5362758d71f5b7614f
Benjamin Hinchliff <benjamin.hinchliff@gmail.com>
2024-03-14 14:00:33 -0700

    add templates and setup for model integration

--- app.py
+++ app.py
@@ -1,54 +1,64 @@
 import os
-from flask import Flask, flash, request, redirect, url_for
+from flask import (
+    Flask,
+    flash,
+    render_template,
+    request,
+    redirect,
+    url_for,
+    send_from_directory,
+)
 from werkzeug.utils import secure_filename
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 
 
-UPLOAD_FOLDER = 'images'
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+UPLOAD_FOLDER = "images"
+ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "gif"}
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+
+app.add_url_rule("/uploads/<name>", endpoint="download_file", build_only=True)
 
 
 def allowed_file(filename):
-    print(filename)
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/', methods=['GET', 'POST'])
 
-new_model = keras.models.load_model('my_model.h5')
+@app.route("/uploads/<name>")
+def download_file(name):
+    return send_from_directory(app.config["UPLOAD_FOLDER"], name)
 
+
+@app.route("/results/<name>", methods=["GET"])
+def display_results(name):
+    return render_template("results.html", name=name)
+
+
+@app.route("/", methods=["GET", "POST"])
 def upload_file():
-    if request.method == 'POST':
+    if request.method == "POST":
         # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
+        if "file" not in request.files:
+            flash("No file part")
             return redirect(request.url)
-        file = request.files['file']
+        file = request.files["file"]
         # If the user does not select a file, the browser submits an
         # empty file without a filename.
-        if file.filename == '':
-            flash('No selected file')
+        if file.filename is None or file.filename == "":
+            flash("No selected file")
             return redirect(request.url)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            print(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            
-    return '''
-    <!doctype html>
-    <title>Fashion detect</title>
-    <h1>Upload image below!</h1>
-    <form method=post enctype=multipart/form-data>
-      <input type=file name=file>
-      <input type=submit value=Upload>
-    </form>
-    '''
+            file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+            return redirect(url_for("display_results", name=filename))
+
+    return render_template("index.html")
+
 
 def classify_image(image_path):
     # fashion, brands, gen
     pass
+
