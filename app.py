f2ef363c731725ca7696c4c53f46bf567391859c
Benjamin Hinchliff <benjamin.hinchliff@gmail.com>
2024-03-22 15:57:07 -0700

    add final websever changes to repo

--- app.py
+++ app.py
@@ -32,7 +32,19 @@ STRATA_CLASS_NAMES = {
     "Footwear": ['adidas', 'arrow', 'asics', 'basics', 'buckaroo', 'calvin klein', 'carlton london', 'catwalk', 'clarks', 'cobblerz', 'converse', 'coolers', 'crocs', 'disney', 'elle', 'enroute', 'estd', 'f sports', 'fila', 'flying machine', 'force 10', 'franco leone', 'ganuchi', 'gas', 'globalite', 'grendha', 'hm', 'hush puppies', 'id', 'inc', 'ipanema', 'lee', 'lotto', 'marvel', 'mr', 'nike', 'numero uno', 'only', 'playboy', 'portia', 'provogue', 'puma', 'quechua', 'quiksilver', 'red chief', 'red tape', 'reebok', 'rocia', 'rockport', 'roxy', 'senorita', 'skechers', 'spinn', 'timberland', 'tiptopp', 'united colors of benetton', 'unk', 'vans', 'warner bros kids', 'woodland', 'wrangler']
 }
 
+STRATA_TYPE_MODEL_PATHS = {
+    "Apparel": "cp-Apparel-type.weights.h5",
+    "Accessories": "cp-Accessories-type.weights.h5",
+    "Footwear": "cp-Footwear-type.weights.h5",
+}
+STRATA_TYPE_CLASS_NAMES = {
+    "Apparel": ['Apparel Set_Clothing Set', 'Apparel Set_Kurta Sets', 'Apparel Set_Swimwear', 'Bottomwear_Capris', 'Bottomwear_Churidar', 'Bottomwear_Jeans', 'Bottomwear_Jeggings', 'Bottomwear_Leggings', 'Bottomwear_Patiala', 'Bottomwear_Rain Trousers', 'Bottomwear_Salwar', 'Bottomwear_Salwar and Dupatta', 'Bottomwear_Shorts', 'Bottomwear_Skirts', 'Bottomwear_Stockings', 'Bottomwear_Swimwear', 'Bottomwear_Tights', 'Bottomwear_Track Pants', 'Bottomwear_Tracksuits', 'Bottomwear_Trousers', 'Dress_Dresses', 'Dress_Jumpsuit', 'Innerwear_Boxers', 'Innerwear_Bra', 'Innerwear_Briefs', 'Innerwear_Camisoles', 'Innerwear_Innerwear Vests', 'Innerwear_Shapewear', 'Innerwear_Trunk', 'Loungewear and Nightwear_Baby Dolls', 'Loungewear and Nightwear_Bath Robe', 'Loungewear and Nightwear_Lounge Pants', 'Loungewear and Nightwear_Lounge Shorts', 'Loungewear and Nightwear_Lounge Tshirts', 'Loungewear and Nightwear_Night suits', 'Loungewear and Nightwear_Nightdress', 'Loungewear and Nightwear_Robe', 'Loungewear and Nightwear_Shorts', 'Saree_Sarees', 'Socks_Booties', 'Topwear_Belts', 'Topwear_Blazers', 'Topwear_Dresses', 'Topwear_Dupatta', 'Topwear_Jackets', 'Topwear_Kurtas', 'Topwear_Kurtis', 'Topwear_Lehenga Choli', 'Topwear_Nehru Jackets', 'Topwear_Rain Jacket', 'Topwear_Rompers', 'Topwear_Shirts', 'Topwear_Shrug', 'Topwear_Suspenders', 'Topwear_Sweaters', 'Topwear_Sweatshirts', 'Topwear_Tops', 'Topwear_Tshirts', 'Topwear_Tunics', 'Topwear_Waistcoat'],
+    "Accessories": ['Accessories_Accessory Gift Set', 'Accessories_Hair Accessory', 'Accessories_Key chain', 'Accessories_Messenger Bag', 'Accessories_Travel Accessory', 'Accessories_Water Bottle', 'Bags_Backpacks', 'Bags_Clutches', 'Bags_Duffel Bag', 'Bags_Handbags', 'Bags_Laptop Bag', 'Bags_Messenger Bag', 'Bags_Mobile Pouch', 'Bags_Rucksacks', 'Bags_Tablet Sleeve', 'Bags_Travel Accessory', 'Bags_Trolley Bag', 'Bags_Waist Pouch', 'Bags_Wallets', 'Belts_Belts', 'Belts_Tshirts', 'Cufflinks_Cufflinks', 'Cufflinks_Ties and Cufflinks', 'Eyewear_Sunglasses', 'Gloves_Gloves', 'Headwear_Caps', 'Headwear_Hat', 'Headwear_Headband', 'Jewellery_Bangle', 'Jewellery_Bracelet', 'Jewellery_Earrings', 'Jewellery_Jewellery Set', 'Jewellery_Necklace and Chains', 'Jewellery_Pendant', 'Jewellery_Ring', 'Mufflers_Mufflers', 'Perfumes_Perfume and Body Mist', 'Scarves_Scarves', 'Shoe Accessories_Shoe Accessories', 'Shoe Accessories_Shoe Laces', 'Socks_Socks', 'Sports Accessories_Wristbands', 'Stoles_Stoles', 'Ties_Ties', 'Umbrellas_Umbrellas', 'Wallets_Wallets', 'Watches_Watches', 'Water Bottle_Water Bottle'],
+    "Footwear": ['Flip Flops_Flip Flops', 'Sandal_Flip Flops', 'Sandal_Sandals', 'Sandal_Sports Sandals', 'Shoes_Casual Shoes', 'Shoes_Flats', 'Shoes_Formal Shoes', 'Shoes_Heels', 'Shoes_Sandals', 'Shoes_Sports Shoes']
+}
+
 app = Flask(__name__)
+app.secret_key = "/9/tTUgY0Offr1LHMFND"
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 app.add_url_rule("/uploads/<name>", endpoint="download_file", build_only=True)
@@ -47,9 +59,10 @@ def download_file(name):
     return send_from_directory(app.config["UPLOAD_FOLDER"], name)
 
 
-def create_model(num_classes):
+def create_model(num_classes, oneohone=False):
     input_shape=(60, 60, 3)
-    model = keras.applications.ResNet50(  # Add the rest of the model
+    m = keras.applications.ResNet101 if oneohone else keras.applications.ResNet50
+    model = m(  # Add the rest of the model
         weights=None, input_shape=input_shape, classes=num_classes
     )
     
@@ -60,14 +73,17 @@ def create_model(num_classes):
     )
     return model
 
-def load_model(num_classes, path):
-    model = create_model(num_classes)
+def load_model(num_classes, path, oneohone=False):
+    model = create_model(num_classes, oneohone)
     model.load_weights(path)
     return model
 
 decider_model = load_model(len(DECIDER_CLASS_NAMES), DECIDER_MODEL_PATH)
 strata_models = {
     strata: load_model(len(STRATA_CLASS_NAMES[strata]), path) for strata, path in STRATA_MODEL_PATHS.items()
+}
+strata_type_models = {
+    strata: load_model(len(STRATA_TYPE_CLASS_NAMES[strata]), path) for strata, path in STRATA_TYPE_MODEL_PATHS.items()
 }
 
 
@@ -98,7 +114,10 @@ def classify_image(image_path):
     category_name = DECIDER_CLASS_NAMES[np.argmax(category)]
     brand = strata_models[category_name](img)
     brand_name = STRATA_CLASS_NAMES[category_name][np.argmax(brand)]
-    return f"{category_name} {brand_name}"
+    type_ = strata_type_models[category_name](img)
+    print(type_, STRATA_TYPE_CLASS_NAMES[category_name], np.argmax(type_), len(STRATA_TYPE_CLASS_NAMES[category_name]))
+    type_name = STRATA_TYPE_CLASS_NAMES[category_name][np.argmax(type_)]
+    return f"{category_name} {' '.join(type_name.split('_'))} from {brand_name}"
 
 
 @app.route("/", methods=["GET", "POST"])
